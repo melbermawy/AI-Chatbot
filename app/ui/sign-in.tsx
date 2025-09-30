5188e22c5691a7b7bd9928a7d8f74723d1c1d85a
Mohamad Abdou <mohamedabdouelbermawy@gmail.com>
2025-09-30 12:37:55 -0400

    Chatbot works but still ugly lowkey

--- app/ui/sign-in.tsx
+++ app/ui/sign-in.tsx
@@ -1,15 +1,30 @@
+import { useState } from "react"
 import { Form } from "react-router"
+import { authClient } from "~/auth-client"
 
 export default function Signin() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+   async function handleSignUp(e: React.FormEvent) {
+       e.preventDefault();
+       await authClient.signIn.email({ email, password, callbackURL: "/" })
+   }
 
 
-    return (
-        <div className="border border-white">
-        <Form method="post" action="/api/auth/sign-in">
-        <input className="border border-white" name="email" type="email"  placeholder="Enter Email" required />
-        <input className="border border-white" name="password" type="password" placeholder="Enter Password" required />
-        <button className="border border-white" type="submit">Sign In</button>
-        </Form>
-        </div>
-    )
+   return (
+       <div className="bg-yellow">
+       <form onSubmit={handleSignUp} className="flex flex-row gap-5 p-5 rounded-2xl ">
+       <input className="border border-white" name="email" type="email" placeholder="Enter Email" onChange={(e) => setEmail(e.target.value)} required />
+       <input className="border border-white" name="password" type="password" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)} required />
+       <button className="border border-white" type="submit">Sign In</button>
+       </form>
+       </div>
+
+
+
+
+   )
+
 }
